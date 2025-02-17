28291e0185c3d38cc509f44e135c247ca7024e88
sanketp2001 <sanket.p@angularminds.in>
2025-02-17 12:00:43 +0530

    added middleware cors and cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const connectDB = require("./config/configdb");
+const { connectDB } = require("./config/configdb");
 require("dotenv").config();
 const authRouter = require("./routes/authRouter");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT;
 
-connectDB.connectDB();
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/auth", authRouter);
 
