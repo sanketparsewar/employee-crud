037bb81a2e286272154ce415b07d6065da16f680
sanketp2001 <sanket.p@angularminds.in>
2025-02-17 12:02:23 +0530

    updted the model with role,department,and refreshtoken

---
+++ backend/src/models/employee.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["Employee", "Admin"],
+      default: "Employee",
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Employee", employeeSchema);
