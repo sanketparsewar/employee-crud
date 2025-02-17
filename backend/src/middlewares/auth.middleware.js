babe017e2f69b8b57aa0262f410207900344deb1
sanketp2001 <sanket.p@angularminds.in>
2025-02-17 12:01:23 +0530

    middlware to verify token

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const Employee = require("../models/employee");
+require("dotenv").config();
+
+// Middleware to verify JWT token
+exports.verifyJwt = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({ message: "Token is not provided" });
+    }
+    const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+    const employee = await Employee.findById(decode?._id).select(
+      "-password -refreshToken"
+    );
+    if (!employee) {
+      return res.status(403).json({ message: "Unauthorized access" });
+    }
+    req.employee = employee;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
