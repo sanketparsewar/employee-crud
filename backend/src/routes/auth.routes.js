c33cddda39e2e3ffc56afc2c56da1ea1a70df486
sanketp2001 <sanket.p@angularminds.in>
2025-02-18 10:34:37 +0530

    added refresh token route to auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, logout } = require("../controllers/auth.controller");
+const { register, login, logout,refreshAccessToken } = require("../controllers/auth.controller");
 const { verifyJwt } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.post("/login", login);
 
 // secured Routes
 router.get("/logout", verifyJwt, logout);
+router.post('/refresh-token',refreshAccessToken)
 
 module.exports = router;
