2cbba9acf24a8993e44ae9295c5885d9bddc7808
sanketp2001 <sanket.p@angularminds.in>
2025-02-18 18:55:18 +0530

    added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './core/guards/auth/auth.guard';
+import { employeeGuard } from './core/guards/employee/employee.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent,canActivate:[employeeGuard] },
+
+  // Add other routes here...
+  {
+    path: 'auth',
+    canActivate:[authGuard],
+    children: [
+      { path: 'login', component: LoginComponent},
+      { path: 'register', component: RegisterComponent},
+    ],
+  },
+];
