f113b98fbe6b6ccd5add3456fe2499599400d630
sanketp2001 <sanket.p@angularminds.in>
2025-02-19 18:28:19 +0530

    added toast required providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,12 +2,21 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './core/services/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    BrowserAnimationsModule,
+    provideAnimations(), // required animations providers
+    provideToastr({
+      timeOut: 2000,
+      positionClass: 'toast-bottom-center',
+    }), 
   ],
 };
