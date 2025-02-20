c6e2e8198501f969ed179b991091277bb4c1ac14
sanketp2001 <sanket.p@angularminds.in>
2025-02-20 12:42:16 +0530

    updated

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { Router } from '@angular/router';
 import { EmployeeService } from '../../core/services/employee/employee.service';
@@ -8,9 +8,15 @@ import { AddEmployeeComponent } from '../../shared/reusableComponents/add-employ
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { ToastService } from '../../core/services/toast/toast.service';
+import { EditProfileComponent } from '../../shared/reusableComponents/edit-profile/edit-profile.component';
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FormsModule, AddEmployeeComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    AddEmployeeComponent,
+    EditProfileComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -24,9 +30,13 @@ export class HomeComponent implements OnInit {
     limit: 10,
     page: 1,
   };
-  errorMessage: string = '';
   searchSubject = new Subject<string>();
-  totalPages: number = 0;
+  totalPages: number = 1;
+  totalPagesArray: number[] = [];
+  totalEmployees: number = 0;
+  currentPage: number = 1;
+  // itemsPerPage: number = 10; // Adjust this as needed
+
   constructor(
     private authService: AuthService,
     private employeeService: EmployeeService,
@@ -38,17 +48,23 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  ngOnInit() {
+    this.getLoggedEmployee();
+  }
   onSearch(event: any) {
     this.searchSubject.next(event.target.value);
   }
   onChangeFilter(event: any) {
     this.queryParameters[event.target.name] = event.target.value;
-    console.log(this.queryParameters);
     this.getEmployeeList();
   }
 
-  ngOnInit() {
-    this.getLoggedEmployee();
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
+      this.queryParameters.page = page;
+      this.currentPage = page;
+      this.getEmployeeList();
+    }
   }
 
   getLoggedEmployee() {
@@ -58,8 +74,9 @@ export class HomeComponent implements OnInit {
         this.getEmployeeList();
       },
       error: (error) => {
-        if (error.status !== 401)
+        if (error.status !== 401) {
           this.toastService.showError(error.error?.message);
+        }
       },
     });
   }
@@ -69,7 +86,8 @@ export class HomeComponent implements OnInit {
       next: (res: any) => {
         this.employeeList = res.employees;
         this.totalPages = res.totalPages;
-        this.errorMessage = '';
+        this.getTotalPagesArray();
+        console.log(res);
       },
       error: (error) => {
         if (error.status !== 401)
@@ -78,7 +96,13 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  editEmployee(id: string) {}
+  getTotalPagesArray() {
+    this.totalPagesArray = [];
+    for (let i = 1; i < this.totalPages + 1; i++) {
+      this.totalPagesArray.push(i);
+    }
+  }
+
   deleteEmployee(employee: any) {
     if (confirm(`Are you sure you want to delete ${employee.name}?`)) {
       this.employeeService.deleteEmployee(employee._id).subscribe({
@@ -94,22 +118,6 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  // refreshToken() {
-  //   this.authService.refreshToken().subscribe({
-  //     next: () => {
-  //       this.getLoggedEmployee();
-  //       this.getEmployeeList();
-  //     },
-  //     error: (error) => {
-  //       this.toastService.showError(
-  //         'Session expired. Please login to continue...'
-  //       );
-  //       setTimeout(() => {
-  //         this.logout();
-  //       }, 3000);
-  //     },
-  //   });
-  // }
 
   logout() {
     if (confirm(`Are you sure you want to Logout?`)) {
