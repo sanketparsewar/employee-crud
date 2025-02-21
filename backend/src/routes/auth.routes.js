449ef3dbf5a50e7c0db20f9a1b84e4897d27c313
sanketp2001 <sanket.p@angularminds.in>
2025-02-21 11:00:00 +0530

    updates

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { register, login, logout,refreshAccessToken } = require("../controllers/auth.controller");
+const {
+  register,
+  login,
+  logout,
+  refreshAccessToken,
+} = require("../controllers/auth.controller");
+const { validateEmployee } = require("../middlewares/validate.middleware");
 const router = express.Router();
 
 // auth routes
-router.post("/register", register);
+router.post("/register", validateEmployee, register);
 router.post("/login", login);
 router.get("/logout", logout);
-router.post('/refresh-token',refreshAccessToken)
+router.post("/refresh-token", refreshAccessToken);
 
 module.exports = router;
